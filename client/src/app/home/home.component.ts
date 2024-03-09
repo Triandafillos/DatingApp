cd6618d314e18280b8b0a484e94e1402f44aa5ca
Triandafillos <georgepapadakis1111@gmail.com>
2024-03-08 18:38:21 -0700

    Section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,22 +9,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) {  }
+  constructor() {  }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get('http://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error=> {
-      console.log(error);
-    })
   }
 
   cancelRegisterMode(event: boolean) {
